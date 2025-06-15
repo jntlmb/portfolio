2fabc8c6efff09a56952b83f05a1fe4689e12d64
Jonathan <jonathan.leimbach2408@gmail.com>
2025-06-16 00:24:28 +0200

    Updated font subsets

--- src/components/fonts.ts
+++ src/components/fonts.ts
@@ -7,16 +7,20 @@ import {
 
 export const merriweather = Merriweather({
   weight: ['400'],
+  subsets: ['latin'],
 });
 
 export const eduQLD = Edu_QLD_Beginner({
   weight: ['400'],
+  subsets: ['latin'],
 });
 
 export const zillaSlab = Zilla_Slab({
   weight: ['400'],
+  subsets: ['latin'],
 });
 
 export const ibmPlexMono = IBM_Plex_Mono({
   weight: ['300'],
+  subsets: ['latin'],
 });
