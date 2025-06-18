3875570bb778335436b9f323db23f0d32acad4cf
Jonathan Leimbach <jonathan.leimbach2408@gmail.com>
2025-06-18 17:01:13 +0200

    adjusted background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`flex flex-col p-6 antialiased absolute inset-0 h-full w-full bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]`}
+        className={`flex flex-col p-6 antialiased absolute inset-0 h-full w-full bg-neutral-900 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]`}
       >
         <Nav />
         <div className="flex-grow">{children}</div>
